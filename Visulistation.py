81948e623ea9fd6ceba2338d0a83e53a56b7ede5
vijval1410 <63463705+vijval1410@users.noreply.github.com>
2021-06-16 10:32:19 +0530

    Add files via upload

---
+++ Visulistation.py
@@ -0,0 +1,13 @@
+import csv
+import pandas as pd 
+import plotly.graph_objects as go 
+
+df = pd.read_csv("data.csv")
+print (df.groupby("level")["attempt"].mean())
+
+fig = go.Figure(go.Scatter(
+        x = df.groupby("level")["attempt"].mean(),
+        y = ['Level 1','Level 2','Level 3','Level 4'],
+        orientation = 'h'))
+
+fig.show()
